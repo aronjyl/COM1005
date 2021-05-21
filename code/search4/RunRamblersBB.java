d9fa31b6984da9106a06376e39ba8b0c12bc11c4
aronjyl <72772169+aronjyl@users.noreply.github.com>
2021-05-21 01:23:54 +0100

    Update RunRamblersBB.java
    updated bb

--- code/search4/RunRamblersBB.java
+++ code/search4/RunRamblersBB.java
@@ -1,57 +1,64 @@
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class RunRamblersBB {
-    
+public class RunRamblersBB {    
     public static void main(String[] arg){
-        // diablo.pgm
+        // diablo.pgm  && tmc.pgm
         TerrainMap tm = new TerrainMap("tmc.pgm");
 
         int matrix_width = tm.getWidth();
         int matrix_depth = tm.getDepth();
+        // evaluate N times
+        int EXP_NUMs = 1000;
+        float bb_accum_res = 0.0f;
+        ArrayList<Float> bb_res_list = new ArrayList<Float>();
 
-        int goal_w = ThreadLocalRandom.current().nextInt(0, matrix_width);
-        int goal_d = ThreadLocalRandom.current().nextInt(0, matrix_depth);
+        for (int i=0; i<EXP_NUMs; ++i){
+            int goal_d = ThreadLocalRandom.current().nextInt(0, matrix_depth);
+            int goal_w = ThreadLocalRandom.current().nextInt(0, matrix_width);
+    
+            int init_d = ThreadLocalRandom.current().nextInt(0, matrix_depth);
+            int init_w = ThreadLocalRandom.current().nextInt(0, matrix_width);
+    
+            // System.out.println("start x: " + init_d);
+            // System.out.println("start y: " + init_w);
+    
+            // System.out.println("end x: " + goal_d);
+            // System.out.println("end y: " + goal_w);
+    
+            /*
+            1. init a number of start/end points on tmc map
+            */ 
+            Coords goal = new Coords(goal_d, goal_w);
+            int tmap[][] = tm.getTmap();
+            /*
+            2. evaluating these points for branch_bound
+            */
+            RamblersSearch rambler_searcher = new RamblersSearch(tm, goal);
+            SearchState initState = (SearchState) new RamblersState(new Coords(init_d, init_w), 
+                                                                    tmap[init_d][init_w], 0, 0);  // 0 for eucledian 
+    
+            float res_branchbound = rambler_searcher.runSearchE(initState, "branchAndBound");
+            bb_res_list.add(res_branchbound);
 
-        int init_w = ThreadLocalRandom.current().nextInt(0, matrix_width);
-        int init_d = ThreadLocalRandom.current().nextInt(0, matrix_depth);
-        System.out.println(goal_w);
-        System.out.println(goal_d);
-        System.out.println(init_w);
-        System.out.println(init_d);        
+            bb_accum_res += res_branchbound;
+            // System.out.println("branchAndBound : " + res_branchbound);
+        }
+        System.out.println("\nB&B's avg for " + EXP_NUMs + " times: " + bb_accum_res/EXP_NUMs + 
+                    " Standard Deviation: " + calculateSD(bb_res_list));
+    }     
 
-        /*
-        1. init a number of start/end points on tmc map
-        */ 
-        Coords goal = new Coords(goal_w, goal_d);
-        int tmap[][] = tm.getTmap();
-        /*
-        2. evaluating these points for branch_bound
-        */
-        RamblersSearch rambler_searcher = new RamblersSearch(tm, goal);
-        SearchState initState = (SearchState) new RamblersState(new Coords(init_w, init_d), 
-                                                                tmap[init_w][init_d], 0);
-
-        float res_branchbound = rambler_searcher.runSearchE(initState, "breadthFirst");
-        System.out.println(res_branchbound);
-        
-        /*
-        3. evaluting A* search for 
-            1) manhattan;
-            2) euclidean;
-            3) height diff;
-            4) combination all
-        */        
-        float res_astar = rambler_searcher.runSearchE(initState, "AStar");
-        System.out.println(res_astar);
-
-        /*
-        4. compare the efficiency of branch&bound and A*
-        */
-
-        /*
-        5. report to a) describe B&B and A*; b) present results; 3) comparison results
-        */
-        
+    public static double calculateSD(ArrayList<Float> numList){
+        float sum = 0.0f, standardDeviation = 0.0f;
+        int length = numList.size();
+        for(float num : numList) {
+            sum += num;
+        }
+        float mean = sum/length;
+        for(float num: numList) {
+            standardDeviation += Math.pow(num - mean, 2);
+        }
+        return Math.sqrt(standardDeviation/length);
     }
+
 }
